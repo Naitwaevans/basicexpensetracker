91e0395ceb321e020418e5183776299f2db7ba87
Rabbits Foot <evansmaraah@gmail.com>
2023-05-18 11:26:01 +0300

    stylecompleted

---
+++ script.js
@@ -0,0 +1,21 @@
+const form = document.getElementById("form");
+const inputText = document.getElementById("inpText");
+
+
+
+
+
+
+form.addEventListener("submit",  (inputText)=>{
+    inputText.preventDefault()
+    if
+    (inputText){
+        inputText.value =""
+        console.log('failure')
+    }
+else{
+        console.log('success')
+
+    }
+
+});
