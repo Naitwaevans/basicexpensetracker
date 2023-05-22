dd7e17d13f1a29e5d4e3fa3a9691103b6fafcc9f
Rabbits Foot <evansmaraah@gmail.com>
2023-05-22 09:49:41 +0300

    Few changes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("form");
 const inputText = document.getElementById("inpText");
+const msg = document.getElementById("msg")
 
 
 
@@ -10,6 +11,7 @@ form.addEventListener("submit",  (e)=>{
     e.preventDefault()
     if (inputText.value === "") {
         console.log('failure');
+        msg.innerHTML = "Item cannot be blank!"
     } else {
         console.log('Success');
     }
